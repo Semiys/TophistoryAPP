9bece4c32008644f913148cc560f861f3b566b61
Semiys <Semiysgladiys@gmail.com>
2024-03-19 22:01:18 +0400

    Добавлен ( о проекте)

--- app/src/main/java/com/example/tophistoryapp/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/tophistoryapp/ui/slideshow/SlideshowFragment.kt
@@ -1,5 +1,6 @@
 package com.example.tophistoryapp.ui.slideshow
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +17,7 @@ class SlideshowFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,13 +27,18 @@ class SlideshowFragment : Fragment() {
 
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        binding.textViewAbout3.text = "Дорогие друзья, курс на социально-ориентированный национальный проект в значительной степени обуславливает создание системы масштабного изменения ряда параметров. Не следует, однако, забывать о том, что повышение уровня гражданского сознания создаёт предпосылки качественно новых шагов для существующих финансовых и административных условий. Задача организации, в особенности же новая модель организационной деятельности позволяет выполнить важнейшие задания по разработке ключевых компонентов планируемого обновления.\n" +
+                "\n" +
+                "Таким образом, постоянный количественный рост и сфера нашей активности играет важную роль в формировании системы обучения кадров, соответствующей насущным потребностям. Дорогие друзья, дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание соответствующих условий активизации. Таким образом, постоянный количественный рост и сфера нашей активности играет важную роль в формировании модели развития. Соображения высшего порядка, а также сложившаяся структура организации способствует подготовке и реализации дальнейших направлений развития проекта."
 
 
         return root
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
