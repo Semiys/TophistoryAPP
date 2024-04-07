0b49a67768d11843d26d45fda958293080dba242
Semiys <Semiysgladiys@gmail.com>
2024-04-07 20:36:07 +0400

    Заполнены все улицы с сайта

--- app/src/main/java/com/example/tophistoryapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/tophistoryapp/ui/gallery/GalleryFragment.kt
@@ -49,8 +49,44 @@ class GalleryFragment : Fragment(), GalleryAdapter.OnItemClickListener {
             GalleryAdapter.StreetItem("Улица Прокофьева", getString(R.string.prokofev), R.drawable.prokofiev),
             GalleryAdapter.StreetItem("Улица Хваткова", getString(R.string.hvatkov), R.drawable.hvatkova),
             GalleryAdapter.StreetItem("Улица Черняховского", getString(R.string.chernyahovskiy), R.drawable.chernyxovsk),
-            GalleryAdapter.StreetItem("Улица Шигаева", getString(R.string.shigaev), R.drawable.shigaev)
+            GalleryAdapter.StreetItem("Улица Шигаева", getString(R.string.shigaev), R.drawable.shigaev),
             // Засвияжский район закрыт.
+            // Заволжский район открыт.
+            GalleryAdapter.StreetItem("Улица Алексея Наганова", getString(R.string.naganov), R.drawable.naganovv),
+            GalleryAdapter.StreetItem("Улица Врача Михайлова", getString(R.string.mixailovv), R.drawable.mixailov),
+            GalleryAdapter.StreetItem("Улица Генерала Кашубы", getString(R.string.kashubas), R.drawable.kashuba),
+            GalleryAdapter.StreetItem("Проспект Генерала Маргелова", getString(R.string.margelovv), R.drawable.margelov),
+            GalleryAdapter.StreetItem("Проспект Генерала Тюленева", getString(R.string.tulenevv), R.drawable.tulenev),
+            GalleryAdapter.StreetItem("Улица Гусева", getString(R.string.gusevv), R.drawable.gusev),
+            GalleryAdapter.StreetItem("Улица Деева", getString(R.string.deevv), R.drawable.deev),
+            GalleryAdapter.StreetItem("Улица Карбышева", getString(R.string.karbishevv), R.drawable.karbishev),
+            GalleryAdapter.StreetItem("Улица Лизы Чайкиной", getString(R.string.chaikinaa), R.drawable.chaikina),
+            GalleryAdapter.StreetItem("Улица Марии Мусоровой", getString(R.string.musorоvaa), R.drawable.musorova),
+            GalleryAdapter.StreetItem("Переулок Тузова", getString(R.string.tuzov), R.drawable.portrait),
+            GalleryAdapter.StreetItem("Улица Шурова", getString(R.string.shurovv), R.drawable.shurov),
+            // Заволжский район закрыт.
+            // Железнодорожный район открыт.
+            GalleryAdapter.StreetItem("Улица Героев Свири", getString(R.string.baryshevv), R.drawable.baryshev),
+            GalleryAdapter.StreetItem("Проезд Героя России Аверьянова", getString(R.string.averinovaa), R.drawable.averinova),
+            GalleryAdapter.StreetItem("Улица Каштанкина", getString(R.string.kashtankinn), R.drawable.kashtankin),
+            GalleryAdapter.StreetItem("Улица Расковой", getString(R.string.rackovaa), R.drawable.rackova),
+            GalleryAdapter.StreetItem("Улица Хазова", getString(R.string.hazovaa), R.drawable.hazova),
+            // Железнодорожный район закрыт.
+            // Ленинский район открыт.
+            GalleryAdapter.StreetItem("Улица Александра Матросова", getString(R.string.matrosovv), R.drawable.matrosov),
+            GalleryAdapter.StreetItem("Улица Ватутина", getString(R.string.vatutinn), R.drawable.vatutin),
+            GalleryAdapter.StreetItem("Улица Генерала Табакина", getString(R.string.tabakinn), R.drawable.tabakin),
+            GalleryAdapter.StreetItem("Улица Зои Космодемьянской", getString(R.string.kosmoss), R.drawable.kosmos),
+            GalleryAdapter.StreetItem("Улица Корюкина", getString(R.string.korushkinaa), R.drawable.korushkina),
+            GalleryAdapter.StreetItem("Улица Любови Шевцовой", getString(R.string.shevcovaa), R.drawable.shevcova),
+            GalleryAdapter.StreetItem("Улица Олега Кошевого", getString(R.string.koshegovaa), R.drawable.koshegova),
+            GalleryAdapter.StreetItem("Улица Сергея Тюленина", getString(R.string.tulenevvsadsaasd), R.drawable.tulenevvsadsa),
+            GalleryAdapter.StreetItem("Улица Толбухина", getString(R.string.tolbixinaa), R.drawable.tolbixina),
+            GalleryAdapter.StreetItem("Улица Ульяны Громовой", getString(R.string.gromovaa), R.drawable.gromova)
+            // Ленинский район закрыт.
+
+
+
             // ... добавьте остальные элементы
         )
 
